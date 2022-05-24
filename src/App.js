9c2b0b3ca7184315b1d4cd7b349e15ba5d3a8e0a
Can Kanbur <cankanbur1@gmail.com>
2022-05-24 21:26:54 +0300

    added sample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import List from './component/List';
+import React, { useState, useEffect } from 'react';
+import { arr } from './data';
 function App() {
+  const [data, setData] = useState(arr);
+  const deletePerson = (id) => {
+    setData(prev => prev.filter(person => person.id !== id));
+  }
+  useEffect(() => {
+    let timer;
+    if (data.length === 0) {
+      timer = setTimeout(() => {
+        setData(prev => {
+          return [...prev, ...arr];
+        });
+      }, 2000);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className="container">
+        <h2>{data.length} birthdays reminder</h2>
+        <List people={data} deletePerson={deletePerson}/>
+        <button onClick={() => setData([])}>Delete all reminder</button>
+      </section>
+    </main>
   );
 }
 
